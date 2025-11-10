d96744115c0ef6350896005183abf4c338f87c61
Kim Hyo Chan <skkhc0317@gmail.com>
2025-11-11 02:44:18 +0900

    feature(layout): update App.vue

--- v01/src/App.vue
+++ v01/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <!-- Header 파일 상단 고정 -->
-  <Header></Header>
+  <Header />
 
   <hr>
 
   <!-- 라우터로 연결되는 views 폴더의 파일 렌더링 -->
-  <router-view></router-view>
+  <router-view />
 
   <hr>
 
   <!-- Footer 파일 하단 고정 -->
-   <Footer></Footer>
+   <Footer />
 </template>
 
 <script setup>
