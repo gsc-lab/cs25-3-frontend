85ccbc9ab9a319904bc70021939504db32d7a03b
Kim Hyo Chan <skkhc0317@gmail.com>
2025-11-12 01:17:30 +0900

    test(props-emit): add defineProps and defineEmits directions

--- v01/src/router/index.js
+++ v01/src/router/index.js
@@ -1,13 +1,13 @@
-// vue-router에서 createApp, createWebHistory 함수 불러오기
-import { createApp, createWebHistory } from 'vue-router'
+// vue-router에서 createRouter, createWebHistory 함수 불러오기
+import { createRouter, createWebHistory } from 'vue-router'
 
 // 라우터 URL과 연결할 view 컴포넌트 가져오기
-// import Main from '@/views/Main.vue'
+import Test from "@/views/users/Test.vue";
 
 // 라우터 설정
-export default createApp({
+export default createRouter({
     history: createWebHistory(),
     routes: [
-        // { path: '/', component: Main}
+        { path: "/test", name: "test", component: Test }
     ]
 })
