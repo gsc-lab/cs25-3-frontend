7b897fa113a9ec8063b16b2e475987f631f94e53
HyoChan Kim <skkhc0317@gmail.com>
2025-11-13 23:05:53 +0900

    chore(vite.config.js): update proxy server's name (#10)

--- v01/vite.config.js
+++ v01/vite.config.js
@@ -14,11 +14,11 @@ export default defineConfig({
   // 개발 서버 설정
   server: {
     proxy: {
-      // '/api'로 시작하는 요청을 프록시 처리
-      '/api': {
+      // '/v1'로 시작하는 요청을 프록시 처리
+      '/v1': {
         target: 'http://localhost:80', // 백엔드 서버 주소
         changeOrigin: true,  // 요청 헤더의 Origin을 target으로 변경 -> CORS 방지
-        rewrite: (path) => path.replace(/^\/api/, ''), // 경로 재작성: '/api' 제거
+        // rewrite: (path) => path.replace(/^\/api/, ''), // 경로 재작성: '/api' 제거
       },
     },
   },
