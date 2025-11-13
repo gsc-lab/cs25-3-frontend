7d6f37fb3e66c4c28a18221c794cf27dbb9075d2
Kim Hyo Chan <skkhc0317@gmail.com>
2025-11-13 22:44:08 +0900

    feature(users): add piniaPluginPersistedstate to stay logged in

--- v01/src/main.js
+++ v01/src/main.js
@@ -1,9 +1,20 @@
-import { createApp } from 'vue'      // createApp 함수 가져오기
-import { createPinia } from 'pinia'  // store 생성용 함수 가져오기
-import App from './App.vue'          // 최상위 루트 컴포넌트 가져오기
-import router from './router'        // 라우터 설정 파일 가져오기
+// src/main.js
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App)           // Vue 앱 인스턴스 생성
-    .use(createPinia())  // 전역 상태 관리(Pinia) 연결
-    .use(router)         // 라우터 연결
-    .mount('#app')       // index.html 안의 id="app" 요소에 앱 마운트
+const app = createApp(App);
+
+// 1. pinia 인스턴스 하나 생성
+const pinia = createPinia();
+
+// 2. 여기에 플러그인 붙이기
+pinia.use(piniaPluginPersistedstate);
+
+// 3. 이 pinia를 앱에 등록
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
