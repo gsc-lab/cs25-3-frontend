33fb6debcc97fd3ddd74ccaea84dab48b02ec2ba
Kim Hyo Chan <skkhc0317@gmail.com>
2025-11-13 22:45:59 +0900

    feature(users): add users router

--- v01/src/router/index.js
+++ v01/src/router/index.js
@@ -1,13 +1,20 @@
 // vue-router에서 createApp, createWebHistory 함수 불러오기
-import { createApp, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // 라우터 URL과 연결할 view 컴포넌트 가져오기
 // import Main from '@/views/Main.vue'
+import UsersRegister from "@/views/users/UsersRegister.vue";
+import UsersLogin from "@/views/users/UsersLogin.vue";
+import UsersLogout from "@/views/users/UsersLogout.vue";
+import Mypage from "@/views/mypage/Mypage.vue";
 
 // 라우터 설정
-export default createApp({
+export default createRouter({
     history: createWebHistory(),
     routes: [
-        // { path: '/', component: Main}
+        { path: '/users/create', component: UsersRegister },
+        { path: '/users/login', component: UsersLogin },
+        { path: '/users/logout', component: UsersLogout},
+        { path: '/mypage', component: Mypage.vue },
     ]
-})
+});
