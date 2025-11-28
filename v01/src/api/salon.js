fb9fe8eb53a5e5f19707f1768d7aba1431777040
HyoChan Kim <skkhc0317@gmail.com>
2025-11-28 23:36:53 +0900

    fix(salon): update salon.js (api) (#41)

--- v01/src/api/salon.js
+++ v01/src/api/salon.js
@@ -2,23 +2,15 @@ import { request } from './client';
 
 // salon API
 export const SalonApi = {
-    // 전체 조회
-    async list() {
-        const res = await request(
-            "GET",
-            "/salon"
-        );
-        // success, data
-        return res.data;
-    },
-    // 수정하기
-    async update(payload) {
-        const res = await request(
-            "PUT",
-            "/salon/update",
-            payload
-        );
-        // success, data
-        return res.data;
-    }
-}
+  // 전체 조회
+  async list() {
+    const res = await request("GET", "/salon");
+    return res.data;
+  },
+
+  // 수정하기
+  async update(formData) {
+    const res = await request("PUT", "/salon/update", formData);
+    return res.data;
+  }
+};
